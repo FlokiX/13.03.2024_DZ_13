d797d1b5e0e43c58f36808e8652fe703df07a3d3
Denis <124783320+FlokiX@users.noreply.github.com>
2024-03-08 13:07:18 +0300

    дз 13, 1.0

---
+++ CreditCard.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    public class CreditCard
+    {
+       
+        private string cardNumber;
+        private string cardholderName;
+        private DateTime expirationDate;
+        private string pin;
+        private decimal creditLimit;
+        private decimal balance;
+
+        
+        public event EventHandler<decimal> AccountReplenishment;      // Пополнение счета
+        public event EventHandler<decimal> MoneySpending;             // Расход денег
+        public event EventHandler CreditMoneyUsageStarted;            // Старт использования кредитных денег
+        public event EventHandler<decimal> TargetBalanceReached;      // Достижение заданной суммы денег
+        public event EventHandler PinChanged;                         // Смена PIN
+
+      
+        public CreditCard(string cardNumber, string cardholderName, DateTime expirationDate, string pin, decimal creditLimit)
+        {
+            this.cardNumber = cardNumber;
+            this.cardholderName = cardholderName;
+            this.expirationDate = expirationDate;
+            this.pin = pin;
+            this.creditLimit = creditLimit;
+            this.balance = 0;
+        }
+
+      
+        public void ReplenishAccount(decimal amount)
+        {
+            balance += amount;
+            AccountReplenishment?.Invoke(this, amount);
+        }
+
+        
+        public void SpendMoney(decimal amount)
+        {
+            balance -= amount;
+            MoneySpending?.Invoke(this, amount);
+        }
+
+      
+        public void StartUsingCreditMoney()
+        {
+            CreditMoneyUsageStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        
+        public void CheckTargetBalance(decimal targetAmount)
+        {
+            if (balance >= targetAmount)
+            {
+                TargetBalanceReached?.Invoke(this, targetAmount);
+            }
+        }
+
+        public void ChangePin(string newPin)
+        {
+            pin = newPin;
+            PinChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CreditCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard creditCard = new CreditCard("1234567890123456", "John Doe", new DateTime(2025, 12, 31), "1234", 1000);
+
+
+            creditCard.AccountReplenishment += (sender, amount) =>
+                Console.WriteLine("Пополнение счета: +" + amount + "$");
+            creditCard.MoneySpending += (sender, amount) =>
+                Console.WriteLine("Расход денег: -" + amount + "$");
+            creditCard.CreditMoneyUsageStarted += (sender, e) =>
+                Console.WriteLine("Начало использования кредитных средств");
+            creditCard.TargetBalanceReached += (sender, targetAmount) =>
+                Console.WriteLine("Достигнута целевая сумма: " + targetAmount + "$");
+            creditCard.PinChanged += (sender, e) =>
+                Console.WriteLine("Пин-код изменен");
+
+           
+            creditCard.ReplenishAccount(500);  
+            creditCard.SpendMoney(200);        
+            creditCard.ReplenishAccount(400);
+            creditCard.CheckTargetBalance(600);
+            creditCard.ChangePin("4321");      
+        }
+    }
+}
